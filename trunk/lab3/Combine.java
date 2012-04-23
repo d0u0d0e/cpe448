91f573b297ab64fef76987311f1faa5637963119
dmethax <dmethax@8dcbbdc5-fc20-d2ff-9031-6ddbf2deaedc>
2012-04-23 00:02:23 +0000

    Added data files, BIO specific BM algorithm working, Combine function reads in fasta files to string

---
+++ trunk/lab3/Combine.java
@@ -0,0 +1,64 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+
+public class Combine
+{
+   public static void main(String args[]) throws java.io.FileNotFoundException
+   {
+      ArrayList<String> fasta = new ArrayList<String>();
+      ArrayList<String> gff = new ArrayList<String>();
+      ArrayList<String> offset = new ArrayList<String>();
+      
+      // fasta files
+      fasta.add("fasta/contig6.txt");
+      fasta.add("fasta/contig7.txt");
+
+      // gff files
+      gff.add("gff/derecta_dot_contig6.0.gff");
+      gff.add("gff/derecta_dot_contig7.0.gff");
+
+      // check file counts
+      if (fasta.size() != gff.size())
+      {  
+         System.out.println("File count does not match\n");
+         return;
+      }
+
+      // loop through array of files
+      for (int i = 0; i < fasta.size()-1; i++)
+      {
+         Scanner scF1 = new Scanner(new File(fasta.get(i)));
+         Scanner scF2 = new Scanner(new File(fasta.get(i+1)));
+         Scanner scG1 = new Scanner(new File(gff.get(i)));
+         Scanner scG2 = new Scanner(new File(gff.get(i+1)));
+         
+         // fasta1
+         String fasta1 = "";
+         scF1.nextLine();
+         while (scF1.hasNextLine())
+         {
+            fasta1 = fasta1 + scF1.nextLine();
+         }
+        
+         // fasta2
+         String fasta2 = "";
+         scF2.nextLine();
+         while (scF2.hasNextLine())
+         {
+            fasta2 = fasta2 + scF2.nextLine();
+         }
+         
+         findOverlap(fasta1, fasta2);
+
+         System.out.println(fasta1);
+         System.out.println(fasta2);
+         break;
+      }      
+   }
+   
+   public static String findOverlap(String s1, String s2)
+   {
+      return "";      
+   }
+}
--- trunk/lab3/Main.java
+++ trunk/lab3/Main.java
@@ -1,8 +1,15 @@
+import java.util.HashMap;
+
 public class Main
 {
    public static void main(String args[])
    {
       BM bm = new BM();
-      bm.BMrun("Hello", "el");
+      
+      int[] res = bm.GoodSuffix("LLO");
+      for (int i : res)
+         System.out.print(i + ", ");
+      System.out.println("");
+      bm.BMrun("BYEHELLO", "HELLO");
    }
 }

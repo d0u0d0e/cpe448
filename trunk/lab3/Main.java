a06961c39d8b9880622bae55d0af5554935faeb0
tupasben <tupasben@8dcbbdc5-fc20-d2ff-9031-6ddbf2deaedc>
2012-04-24 01:13:18 +0000

    runs regular BM now

--- trunk/lab3/Main.java
+++ trunk/lab3/Main.java
@@ -4,6 +4,11 @@ public class Main
 {
    public static void main(String args[])
    {
+String S = "tgtagataagtacttacttacgtcga";
+String P = "gtacttacttac";
+   
+BM.BMrun(S, P);
+
       BioBM bm = new BioBM();
       System.out.println(bm.BMrun("ATGATATTCGATCGAGCG", "TTT"));
    }

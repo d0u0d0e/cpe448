b7f1a4bc0dfa3219ede41a83c73c16ccae26d2fc
dmethax <dmethax@8dcbbdc5-fc20-d2ff-9031-6ddbf2deaedc>
2012-04-24 06:51:47 +0000

    GFF code done, untested

--- trunk/lab3/Combine.java
+++ trunk/lab3/Combine.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
-
+import java.io.IOException;
 import java.lang.StringBuffer;
 import java.util.StringTokenizer;
 import java.util.ArrayList;
@@ -11,31 +11,37 @@ import java.util.Collections;
 
 public class Combine
 {
-   public static void main(String args[]) throws java.io.FileNotFoundException
+   public static void main(String args[]) throws IOException
    {
       ArrayList<String> fastaFiles = new ArrayList<String>();
       ArrayList<String> gffFiles = new ArrayList<String>();
       ArrayList<String> fasta = new ArrayList<String>();
-      ArrayList<ArrayList<Gene>> gff = new ArrayList<ArrayList<Gene>>();
-      ArrayList<String> combinedFastas = new ArrayList<String>();
+      ArrayList<ArrayList<String>> gff = new ArrayList<ArrayList<String>>();
+      ArrayList<String> combinedFASTAS = new ArrayList<String>();
+      ArrayList<ArrayList<String>> combinedGFFS = new ArrayList<ArrayList<String>>();
       int minMatch = 3;
 
       // intialize bio-specific Boyer-Moore's algorithm
       BioBM bm = new BioBM();
 
       // fasta files
-      fastaFiles.add("fasta1");
+      /*fastaFiles.add("fasta1");
       fastaFiles.add("fasta2");
       fastaFiles.add("fasta3");
       fastaFiles.add("fasta4");
       fastaFiles.add("fasta5");
-      
+      */
       // gff files
-      gffFiles.add("gff1");
+      /*gffFiles.add("gff1");
       gffFiles.add("gff2");
       gffFiles.add("gff3");
       gffFiles.add("gff4");
       gffFiles.add("gff5");
+*/
+      fastaFiles.add("fasta/contig6.txt");
+      fastaFiles.add("fasta/contig7.txt");
+      gffFiles.add("gff/derecta_dot_contig6.0.gff");
+      gffFiles.add("gff/derecta_dot_contig7.0.gff");
 
       // check file counts
       if (fastaFiles.size() != gffFiles.size() && fastaFiles.size() > 1)
@@ -53,7 +59,8 @@ public class Combine
       }
 
       // combine files
-      String superFasta = fasta.get(0);
+      String superFASTA = fasta.get(0);
+      ArrayList<String> superGFF = new ArrayList<String>();
 
       for (int i = 0; i < fasta.size()-1; i++)
       {   
@@ -63,8 +70,8 @@ public class Combine
          int end = minMatch;
          
          String fasta2 = fasta.get(i+1);
-         ArrayList<Gene> gff1 = gff.get(i);
-         ArrayList<Gene> gff2 = gff.get(i+1);
+         ArrayList<String> gff1 = gff.get(i);
+         ArrayList<String> gff2 = gff.get(i+1);
         
          if (end > fasta2.length()-1)
          {
@@ -72,18 +79,19 @@ public class Combine
             return;
          }
 
-         while ((overlap = bm.BMrun(superFasta, fasta2.substring(0, end))) >= 0)
+         while ((overlap = bm.BMrun(superFASTA, fasta2.substring(0, end))) >= 0)
          {
             maxOverlap = overlap;
             end++;
-            if (end >= fasta2.length())
+            if (end >= fasta2.length()) 
                break;
          }
+         System.out.println(maxOverlap);
          if (maxOverlap != -1)
          {
             //////////////////////////////
             // QUALITY CHECK, REMOVE LATER
-            if (!fasta2.startsWith(superFasta.substring(maxOverlap, superFasta.length()-1)))
+            if (!fasta2.startsWith(superFASTA.substring(maxOverlap, superFASTA.length()-1)))
             {
                System.out.println("Overlap not actually found at the end");
                return;
@@ -91,26 +99,133 @@ public class Combine
             // QUALITY CHECK, REMOVE LATER
             //////////////////////////////
             
-            offset = superFasta.length() - maxOverlap;
-            superFasta = superFasta + fasta2.substring(offset, fasta2.length());
+            offset = superFASTA.length() - maxOverlap;
+            superFASTA = superFASTA + fasta2.substring(offset, fasta2.length());
+
+            int k = 0;
+
+            for (int j = 0; j < gff1.size(); j++)
+            {
+               ArrayList<String> p1 = parseLine(gff1.get(j));
+               ArrayList<String> p2;
+               int flag = 0;
+               int start1 = Integer.parseInt(p1.get(3));
+               int stop1 = Integer.parseInt(p1.get(4));
+               int start2, stop2;
+               if (p1.get(6).charAt(0) == '+' && start1 >= maxOverlap)
+               {
+                  p2 = parseLine(gff2.get(k));
+                  start2 = Integer.parseInt(p2.get(3));
+                  stop2 = Integer.parseInt(p2.get(4));
+                  if (!(start1 == (start2 + offset) && 
+                        stop1 == (stop2 + offset) &&
+                        p1.get(9).equals(p2.get(9)) && 
+                        p1.get(11).equals(p2.get(11)))) 
+                  {
+                     System.out.println("p1: " + p1);
+                     System.out.println("p2: " + p2);
+
+                     if (Math.abs(start1 - stop1) < 
+                         Math.abs(start2 - stop2 + offset))
+                     {
+                        superGFF.add(repiece(p1));
+                     }
+                     else
+                     {
+                        superGFF.add(updateOffset(p2, offset));
+                     }
+                  }
+                  else
+                  {
+                     superGFF.add(updateOffset(p2, offset));
+                  }
+                  k++; 
+               }
+               else if (p1.get(6).charAt(0) == '-' && stop1 >= maxOverlap)
+               {
+                  if (flag == 0)
+                  {
+                     flag = 1;
+                     while (parseLine(gff2.get(k)).get(6).charAt(0) == '+')
+                     {
+                        superGFF.add(updateOffset(parseLine(gff2.get(k)), offset));
+                        k++;
+                     }
+                  } 
+                  p2 = parseLine(gff2.get(k));
+                  start2 = Integer.parseInt(p2.get(3));
+                  stop2 = Integer.parseInt(p2.get(4));
+                  if (!(start1 == (start2 + offset) && 
+                        stop1 == (stop2 + offset) &&
+                        p1.get(9).equals(p2.get(9)) && 
+                        p1.get(11).equals(p2.get(11)))) 
+                  {
+                     System.out.println("p1: " + p1);
+                     System.out.println("p2: " + p2);
+
+                     if (Math.abs(start1 - stop1) < 
+                         Math.abs(start1 - stop2 + offset))
+                     {
+                        superGFF.add(repiece(p1));
+                     }
+                     else
+                     {
+                        superGFF.add(updateOffset(p2, offset));
+                     }
+                  }
+                  else
+                  {
+                     superGFF.add(updateOffset(p2, offset));
+                  }
+                  k++;
+               }
+               else
+               {
+                  superGFF.add(repiece(p1));
+               }
+            }
+            for (int j = k; i < gff2.size(); i++)
+            {
+               superGFF.add(updateOffset(parseLine(gff2.get(i)), offset));
+            }
          }
          else
          {
-            combinedFastas.add(superFasta);
-            superFasta = fasta.get(i+1); 
+            combinedFASTAS.add(superFASTA);
+            combinedGFFS.add(superGFF); 
+            superGFF = new ArrayList<String>();
+            superFASTA = fasta.get(i+1); 
          }
       }
-      combinedFastas.add(superFasta);
-
-      for (String s : combinedFastas)
+      combinedFASTAS.add(superFASTA);
+      combinedGFFS.add(superGFF); 
+      
+      // output test
+      for (String s : combinedFASTAS)
       {
-         System.out.println("Fasta: " + s);
+         //System.out.println("Fasta: " + s);
+      }
+
+      for (ArrayList<String> l : combinedGFFS)
+      {
+         for (String s : l)
+         {
+            System.out.println(s);
+         }
       }
    }
 
-   public static String parseFASTA(String fileName) throws java.io.FileNotFoundException
+   public static String parseFASTA(String fileName) throws IOException
    {
-      Scanner sc = new Scanner(new File(fileName));
+      Scanner sc;
+      try
+      {
+         sc = new Scanner(new File(fileName));
+      }
+      catch (IOException e)
+      {
+         throw new IOException("File not found");
+      }
       String result = "";
       
       sc.nextLine(); // skip header
@@ -122,189 +237,52 @@ public class Combine
       return result;     
    }
     
-   public static ArrayList<Gene> parseGFF(String fileName, int fastaLength) throws java.io.FileNotFoundException
+   public static ArrayList<String> parseGFF(String fileName, int fastaLength) throws IOException
    {
-      ArrayList<Gene> geneList = new ArrayList<Gene>();
-      ArrayList<String> idList = new ArrayList<String>();
-      Scanner sc = new Scanner(new File(fileName));
-      String id = "";      
-      int startg = 0, stopg = 0;
+      ArrayList<String> lines = new ArrayList<String>();
+      Scanner sc;
+      try
+      {
+         sc = new Scanner(new File(fileName));
+      }
+      catch (IOException e)
+      {
+         throw new IOException("File not found");
+      }
       while (sc.hasNextLine())
       {
          String line = sc.nextLine();
-         StringTokenizer st = new StringTokenizer(line, " \t");
-         int count = 0;
-         int flag = 0;
-         while (st.hasMoreTokens())
-         {
-            String word = st.nextToken();
-            if (count == 2)
-            {
-               if (word.equals("mRNA"))
-               {
-                  flag = 1;
-               }
-               else if (word.equals("CDS"))
-               {
-                  flag = 2;
-               }
-            }
-            else if (count == 3 && flag > 0)
-               startg = Integer.parseInt(word);
-            else if (count == 4 && flag > 0)
-               stopg = Integer.parseInt(word);
-            else if (count == 9 && flag > 0)
-            {
-               StringTokenizer st2 = new StringTokenizer(word, "\";");
-               id = st2.nextToken();
-            }
-            count++;
-         }
-         // mRNA
-         if (flag == 1)
-         { 
-            if (!idList.contains(id))
-            {
-               Gene gene;
-               if (startg < stopg)
-                  gene = new Gene (id, startg, stopg, true);
-               else
-                  gene = new Gene (id, stopg, startg, false);
-
-               geneList.add(gene);
-               idList.add(id);
-            }
-            else
-            {
-               for (Gene g : geneList)
-               {
-                  if (id.equals(g.id))
-                  {
-                     if (Math.abs(startg - stopg) > Math.abs(g.start-g.stop))
-                     { 
-                        Gene ng; 
-                        if (startg < stopg)
-                        {
-                           ng = new Gene(id, startg, stopg, true);
-                        }
-                        else
-                        {
-                           ng = new Gene(id, stopg, startg, false);
-                        }
-                        geneList.set(geneList.indexOf(g), ng);
-                     }
-                     break;
-                  }
-               }
-            }            
-         }
-         // CDS
-         else if (flag == 2)
-         {
-            for (Gene g : geneList)
-            {
-               if (id.equals(g.id))
-               {
-                  int dup = 0;
-
-                  for (Gene p : g.points)
-                  {
-                     if (p.start == startg && p.stop == stopg)
-                     {
-                        dup = 1;
-                     }
-                  }
-                  if (dup == 0)
-                  {
-                     if (startg < stopg)
-                     {
-                        g.points.add(new Gene("Exon", startg, stopg, true));
-                     }
-                     else
-                     {
-                        g.points.add(new Gene("Exon", stopg, startg, false));
-                     }
-                  }
-               }
-            }
-         }
+         lines.add(line);
       }
+      return lines;
+   }
+
+   public static ArrayList<String> parseLine(String s)
+   {
+      ArrayList<String> pieces = new ArrayList<String>();
+      StringTokenizer st = new StringTokenizer(s, " \t\";");
+
+      while (st.hasMoreTokens())
+      {
+         pieces.add(st.nextToken());
+      }
+      return pieces;
+   }
+
+   public static String updateOffset(ArrayList<String> l, int offset)
+   {
+      ArrayList<String> temp = new ArrayList<String>(l);
+      int start = Integer.parseInt(l.get(3));
+      int stop = Integer.parseInt(l.get(4));
       
-      // Exons and Introns within Genes
-      int min;
-      int max;
+      temp.set(3, Integer.toString(start + offset));
+      temp.set(4, Integer.toString(stop + offset));
       
-      for (Gene g : geneList)
-      {
-         Collections.sort(g.points, new Comparator<Gene>()
-            {
-               public int compare(Gene a, Gene b) {
-                  return a.start - b.start;
-               }
-            });
-      }
+      return repiece(temp);
+   }
 
-      for (Gene g : geneList)
-      {
-         min = g.start;
-         max = g.stop;
-         ArrayList<Gene> copy = new ArrayList<Gene>(g.points);
-         for (Gene p : copy)
-         {
-            if (min < p.start)
-            {
-               Gene gene = new Gene("Intron", min, p.start-1, false);
-               g.points.add(gene);       
-            }
-            min = p.stop+1;      
-         } 
-         if (max > min)
-         {
-            Gene gene = new Gene("Intron", min, max, false);
-            g.points.add(gene);
-         }
-         Collections.sort(g.points, new Comparator<Gene>()
-            {
-               public int compare(Gene a, Gene b) {
-                  return a.start - b.start;
-               }
-            });
-      }
-
-      // Genes and Intergenic Regions
-      min = 1;
-      max = fastaLength;
-
-      ArrayList<Gene> geneSort = new ArrayList<Gene>(geneList);
-      Collections.sort(geneSort, new Comparator<Gene>() 
-         {
-            public int compare(Gene a, Gene b) {
-               return a.start - b.start;
-            }
-         });
-
-      for (Gene g : geneSort)
-      {
-         if (min < g.start)
-         {
-            Gene gene = new Gene("Intergenic", min, g.points.get(0).start-1, false);
-            geneList.add(gene);
-         }
-         min = g.points.get(g.points.size()-1).stop+1;
-      }
-      if (max > min)
-      {
-         Gene gene = new Gene("Intergenic", min, max, false);
-         geneList.add(gene);
-      }  
-      
-      Collections.sort(geneList, new Comparator<Gene>() 
-         {
-            public int compare(Gene a, Gene b) {
-               return a.start - b.start;
-            }
-         });
-
-      return geneList;
+   public static String repiece(ArrayList<String> l)
+   {
+      return l.get(0) + "   .  " + l.get(2) + "  " + l.get(3) + "  " + l.get(4) + " .  " + l.get(6) + "  .  gene_id \"" + l.get(9) + "\"; transcript_id \"" + l.get(11) + "\";";
    }
 }

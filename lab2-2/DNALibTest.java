ddf672043285cbb55ca1bef06c320bf42c247ce4
tupasben <tupasben@8dcbbdc5-fc20-d2ff-9031-6ddbf2deaedc>
2012-05-10 16:54:55 +0000

    2 tests left

--- lab2-2/DNALibTest.java
+++ lab2-2/DNALibTest.java
@@ -3,10 +3,11 @@ import java.util.ArrayList;
 public class DNALibTest {
    public static void main(String[] args) {
       String sequence = "ATACTGCGATCATACTACGGCGTCGGATATAGCGCGATAAATTTGGCGCGGCGCTACGACGTACGATACGCTAGTCGTAGCGGGCGCGTTTTCGCGCTCGCGCCATATATATAC";
-      DNALib dna = new 
+                       // I  L  R  S  Y  Y  G  V  G  Y  S  A  I  N  L  A  R  R  Y  D  V  R  Y  A  S  R  S  G  R  V  F  A  L  A  P  Y  I  Y
+      DNALib dna = new
 DNALib(sequence, null);
 
-// I L R S Y Y G V G # S A I N L A R R Y D V R Y A S R S G R V F A L A P Y I Y
+// a = 25 t = 27 c = 30 g = 32
 
       String comp = "TATGACGCTAGTATGATGCCGCAGCCTATATCGCGCTATTTAAACCGCGCCGCGATGCTGCATGCTATGCGATCAGCATCGCCCGCGCAAAAGCGCGAGCGCGGTATATATATG";
       if(!dna.complement(0, sequence.length()).equals(comp))
@@ -19,26 +20,47 @@ DNALib(sequence, null);
       if(dna.GCContent(0, sequence.length()) != (double)62/114 * 100)
          throw new AssertionError("GCContent method failed");
 
-      System.out.println("Histogram for Arginine (R)");
+      System.out.println("Histogram for Isoleucine (I)");
       System.out.println("Expected results:");
-      System.out.println("CGT");
-
+      DNALib.aminoAcid aa = dna.map.get("I");
+      int total = 0;
+      for(int i = 0; i < aa.codons.size(); i++)
+    	  total += aa.codons.get(i).count;
+      
+      System.out.println("ATC   " + aa.codons.get(0).count + "   " + (double)aa.codons.get(0).count / total);
+      System.out.println("ATT   " + aa.codons.get(1).count + "   " + (double)aa.codons.get(1).count / total);
+      System.out.println("ATA   " + aa.codons.get(2).count + "   " + (double)aa.codons.get(2).count / total);
+      
+      System.out.println();
       System.out.println("Actual results:");
-      dna.histogram("R");
-/*
-      ArrayList list = new ArrayList();
-      list.add("ATG");
-      list.add("TTT");
-      System.out.println(dna.frequencyOptimalCodons(list));
+      dna.histogram("I");
+      System.out.println();
 
-      System.out.println(dna.rcsu("TTT"));
-      System.out.println(dna.CAI("TTT"));
-      System.out.println(dna.effectiveNumberCodons());
-      System.out.println(dna.scaledChi2("R"));
 
-      System.out.println(dna.enthropy());
-      //gene density
-*/
+      ArrayList<String> list = new ArrayList<String>();
+      list.add("TTG");
+      list.add("ATA");
+      if(dna.frequencyOptimalCodons(list) != (double)4/6)
+         throw new AssertionError("frequencyOptimalCodons method failed");
+
+      if(dna.rcsu("TTG") != (double)1/((double)3/6))
+         throw new AssertionError("RCSU method failed");
+      
+//      System.out.println(dna.CAI("TTT"));
+//      System.out.println(dna.effectiveNumberCodons());
+
+      if(dna.scaledChi2("I") != (double)6/38)
+    	  throw new AssertionError("scaledChi2 method failed");
+
+      //A = 25  T = 27  C = 30  G = 32
+      double enthropy = -((double)25/114 * Math.log((double)25/114)/Math.log(2) + (double)27/114 * Math.log((double)27/114)/Math.log(2)
+    		                + (double)30/114 * Math.log((double)30/114)/Math.log(2) + (double)32/114*Math.log((double)32/114)/Math.log(2));
+
+      if(dna.enthropy() != enthropy)
+    	  throw new AssertionError("enthropy method failed");
+
+
+
 
       System.out.println("All tests have passed");
    }

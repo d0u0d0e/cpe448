1aa04a506ffeb52867999189fcd5a4b26fcd9b92
dmethax <dmethax@8dcbbdc5-fc20-d2ff-9031-6ddbf2deaedc>
2012-05-15 09:57:34 +0000

    semi-gap implementation, but probably wrong

--- lab3-2/Palindrome.java
+++ lab3-2/Palindrome.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.lang.Math;
 
 public class Palindrome
 {
@@ -9,8 +10,11 @@ public class Palindrome
    int min, max, minGap, maxGap;
    SuffixTree tree;
    ArrayList<String> palindromes;
+   ArrayList<String> gapPalindromes;
    HashMap<String, ArrayList<Integer>> locations1;
    HashMap<String, ArrayList<Integer>> locations2;
+   HashMap<String, ArrayList<Integer>> gapLocations1;
+   HashMap<String, ArrayList<Integer>> gapLocations2;
 
    public Palindrome(String seq, int min, int max, int minGap, int maxGap)
    {
@@ -36,7 +40,32 @@ public class Palindrome
       findPalindromes(tree.root, "");
 
       // gaps
-      // not implemented yet
+      this.gapPalindromes = new ArrayList<String>();
+      this.gapLocations1 = new HashMap<String, ArrayList<Integer>>();
+      this.gapLocations2 = new HashMap<String, ArrayList<Integer>>();
+      findGapPalindromes();
+   }
+
+   public void findGapPalindromes()
+   {
+      for (String s : palindromes)
+      {
+         for (int i = 0; i < locations1.get(s).size(); i++)
+         {
+            int gap = Math.abs(locations1.get(s).get(i) - locations2.get(s).get(i));
+            if (gap >= minGap && gap <= maxGap)
+            {
+               if (!(gapLocations1.keySet().contains(s)))
+               {
+                  gapLocations1.put(s, new ArrayList<Integer>());
+                  gapLocations2.put(s, new ArrayList<Integer>());
+                  gapPalindromes.add(s);
+               }
+               gapLocations1.get(s).add(locations1.get(s).get(i)); 
+               gapLocations2.get(s).add(locations2.get(s).get(i)); 
+            }
+         }
+      }
    }
 
    public void findPalindromes(SuffixTree.node root, String s)
@@ -67,25 +96,27 @@ public class Palindrome
          {
             if (((SuffixTree.Leaf)n).suffixNum.size() == 2)
             {
+               String p = s + n.label;
+               p = p.substring(0, p.length()-1);
                if (!(locations1.keySet().contains(s + n.label)))
                {
-                  locations1.put(s + n.label, new ArrayList<Integer>());
-                  locations2.put(s + n.label, new ArrayList<Integer>());
+                  locations1.put(p, new ArrayList<Integer>());
+                  locations2.put(p, new ArrayList<Integer>());
                }
-               palindromes.add(s + n.label);
-               locations1.get(s + n.label).add(((SuffixTree.Leaf)n).suffixNum.get(1));
-               locations2.get(s + n.label).add(((SuffixTree.Leaf)n).suffixNum.get(2));
+               palindromes.add(p);
+               locations1.get(p).add(((SuffixTree.Leaf)n).suffixNum.get(1));
+               locations2.get(p).add(((SuffixTree.Leaf)n).suffixNum.get(2));
             }
          }
       }
       return;
    }
    
-   public String reverseComplement(String s) 
+   public static String reverseComplement(String s) 
    {
       StringBuffer seqBuffer = new StringBuffer();
       for (int i = 0; i < s.length(); i++) {
-         switch (seq.charAt(i)) {
+         switch (s.charAt(i)) {
             case 'A':
                seqBuffer.append("T");
                break;

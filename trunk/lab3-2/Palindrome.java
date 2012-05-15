e393eba01e2032a6f2976bdee3828f4c862f66fd
dmethax <dmethax@8dcbbdc5-fc20-d2ff-9031-6ddbf2deaedc>
2012-05-15 05:30:48 +0000

    palindrome updates

--- trunk/lab3-2/Palindrome.java
+++ trunk/lab3-2/Palindrome.java
@@ -9,6 +9,8 @@ public class Palindrome
    int min, max, gap;
    SuffixTree tree;
    ArrayList<String> palindromes;
+   HashMap<String, ArrayList<Integer>> locations1;
+   HashMap<String, ArrayList<Integer>> locations2;
 
    public Palindrome(String seq, int min, int max, int gap)
    {
@@ -20,10 +22,14 @@ public class Palindrome
       // create tree
       this.tree = new SuffixTree();
       this.tree.addString(seq);
+      System.out.println("Adding: " + seq);
       this.tree.addString(reverseComplement(seq));
+      System.out.println("Adding: " + reverseComplement(seq));
       
-      // create repeats hashmap and unexpected arraylist
+      // create palindrome hashmap and locations hashmaps
       this.palindromes = new ArrayList<String>();
+      this.locations1 = new HashMap<String, ArrayList<Integer>>();
+      this.locations2 = new HashMap<String, ArrayList<Integer>>();
    }
 
    public void findPalindromes(SuffixTree.node root, String s)
@@ -35,6 +41,7 @@ public class Palindrome
             String concat = s + n.label;
             if (concat.length() >= min && concat.length() <= max)
             {
+               System.out.println("checking: " + concat);
                checkChildren(n, concat);
             }
             findPalindromes(n, concat);
@@ -48,17 +55,26 @@ public class Palindrome
       {
          if (n instanceof SuffixTree.Inner)
          {
+            System.out.println("entering: " + n.label);
             checkChildren(n, s);
          }
          else
          {
             if (((SuffixTree.Leaf)n).suffixNum.size() == 2)
             {
+               System.out.println("palindrome: " + s + n.label + " at [" + ((SuffixTree.Leaf)n).suffixNum.get(1) + ", " + ((SuffixTree.Leaf)n).suffixNum.get(2) + "]");
+               if (!(locations1.keySet().contains(s + n.label)))
+               {
+                  locations1.put(s + n.label, new ArrayList<Integer>());
+                  locations2.put(s + n.label, new ArrayList<Integer>());
+               }
                palindromes.add(s + n.label);
+               locations1.get(s + n.label).add(((SuffixTree.Leaf)n).suffixNum.get(1));
+               locations2.get(s + n.label).add(((SuffixTree.Leaf)n).suffixNum.get(2));
             }
             else
             {
-               System.out.println("debug suffixNum: " + ((SuffixTree.Leaf)n).suffixNum.size());
+               System.out.println("skipping: " + n.label);
             }
          }
       }
